6569fdf9b332d88e1a59d93d29fb3fbc83c75dd9
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-30 16:39:07 +0200

    component

--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
   templateUrl: './saludo.component.html',
-  styleUrls: ['./saludo.component.css']
+  styleUrls: ['./saludo.component.css'],
 })
-export class SaludoComponent {
+export class SaludoComponent implements OnInit {
 
+  name: string = 'David';
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    console.log('SaludoComponent ngOnInit');
+  }
 }
