716300a9c0ec627daa25b11fb62bcd611b8df2c7
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-30 23:47:51 +0200

    ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './components/saludo/saludo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SaludoComponent } from './components/saludo/saludo.component';
     SaludoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
