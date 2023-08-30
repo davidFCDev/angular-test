4f7e75b55e1386aad15613463ff1fa52a3ba063d
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-30 16:26:56 +0200

    saludo comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
+import { SaludoComponent } from './components/saludo/saludo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    SaludoComponent
   ],
   imports: [
     BrowserModule
