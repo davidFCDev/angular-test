6a549f72e3db58bb64bf5ab3b7c6e33bdb76c259
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-21 18:31:35 +0200

    form anidado

--- src/app/components/forms/anidated-form/anidated-form.component.ts
+++ src/app/components/forms/anidated-form/anidated-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Importamos de ReactiveForms para crear un formulario anidado
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-anidated-form',
   templateUrl: './anidated-form.component.html',
-  styleUrls: ['./anidated-form.component.css']
+  styleUrls: ['./anidated-form.component.css'],
 })
-export class AnidatedFormComponent {
+export class AnidatedFormComponent implements OnInit {
+  myAnidatedForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    //Agrupación para teléfono
+    const fixedPhone = this.formBuilder.group({
+      prefix: '',
+      number: '',
+    });
+    const mobilePhone = this.formBuilder.group({
+      prefix: '',
+      number: '',
+    });
+  }
 }
