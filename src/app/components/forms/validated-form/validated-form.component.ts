599127b03b7c515a7c35f6a31acb16b76d70b7c0
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-22 16:06:41 +0200

    validators

--- src/app/components/forms/validated-form/validated-form.component.ts
+++ src/app/components/forms/validated-form/validated-form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-validated-form',
   templateUrl: './validated-form.component.html',
   styleUrls: ['./validated-form.component.css']
 })
-export class ValidatedFormComponent {
+export class ValidatedFormComponent implements OnInit {
+
+  myValidatedForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.myValidatedForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+
+  }
 
 }
+
+
