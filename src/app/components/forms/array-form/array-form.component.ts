a1d62cfbfddeacffa57f876f7d7f2de8a3cf9a58
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-22 15:38:51 +0200

    metodo getter

--- src/app/components/forms/array-form/array-form.component.ts
+++ src/app/components/forms/array-form/array-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-array-form',
@@ -7,12 +7,20 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./array-form.component.css'],
 })
 export class ArrayFormComponent implements OnInit {
-  
+
   myArrayForm: FormGroup = new FormGroup({});
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myArrayForm = this.formBuilder.group({
+      name: '',
+      surname: '',
+      phones: this.formBuilder.array([]), // Inicializamos la lista de teléfonos vacìa
+    });
+  }
 
+  get phonesFormArray(): FormArray {
+    return this.myArrayForm.get('phones') as FormArray;
   }
 }
