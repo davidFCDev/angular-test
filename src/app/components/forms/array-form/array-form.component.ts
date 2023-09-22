4c08338d5c0da2c850596350cd9f3d6538cfd71e
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-22 15:34:17 +0200

    array form

--- src/app/components/forms/array-form/array-form.component.ts
+++ src/app/components/forms/array-form/array-form.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-array-form',
   templateUrl: './array-form.component.html',
-  styleUrls: ['./array-form.component.css']
+  styleUrls: ['./array-form.component.css'],
 })
-export class ArrayFormComponent {
+export class ArrayFormComponent implements OnInit {
+  
+  myArrayForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+
+  }
 }
