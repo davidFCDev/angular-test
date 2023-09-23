84c2d1a68543fa724f511554635e13e84f9af291
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-23 18:01:28 +0200

    change dates

--- src/app/components/example-pipes/example-pipes.component.ts
+++ src/app/components/example-pipes/example-pipes.component.ts
@@ -8,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class ExamplePipesComponent implements OnInit {
 
     dob: Date = new Date(1990, 5, 20);
+    change: boolean = true;
 
     constructor() { }
 
+    get dateFormat() {
+      return this.change ? 'shortDate' : 'fullDate';
+    }
+
     ngOnInit() {
+      this.change = !this.change;
+    }
+
+    changeDate = () => {
+      this.change = !this.change;
     }
 
 }
