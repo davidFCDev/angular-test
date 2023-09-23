5fe2b303eeb706ebd5e6a6d612e335fa2faabcc9
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-23 16:17:29 +0200

    validaciones html

--- src/app/components/forms/validated-form/validated-form.component.ts
+++ src/app/components/forms/validated-form/validated-form.component.ts
@@ -4,39 +4,67 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-validated-form',
   templateUrl: './validated-form.component.html',
-  styleUrls: ['./validated-form.component.css']
+  styleUrls: ['./validated-form.component.css'],
 })
 export class ValidatedFormComponent implements OnInit {
-
   myValidatedForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-
     this.myValidatedForm = this.formBuilder.group({
       name: ['', Validators.required],
       surname: '',
       email: ['', Validators.compose([Validators.required, Validators.email])],
-      age: ['', Validators.compose([Validators.required, Validators.min(18), Validators.max(65)])],
-      password: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
-      accept: ['', Validators.requiredTrue]
+      age: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.min(18),
+          Validators.max(65),
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[a-zA-Z]*'),
+        ]),
+      ],
+      accept: ['', Validators.requiredTrue],
     });
-
   }
 
-  get name() { return this.myValidatedForm.get('name'); }
+  get name() {
+    return this.myValidatedForm.get('name');
+  }
 
-  get surname() { return this.myValidatedForm.get('surname'); }
+  get surname() {
+    return this.myValidatedForm.get('surname');
+  }
 
-  get email() { return this.myValidatedForm.get('email'); }
+  get email() {
+    return this.myValidatedForm.get('email');
+  }
 
-  get age() { return this.myValidatedForm.get('age'); }
+  get age() {
+    return this.myValidatedForm.get('age');
+  }
 
-  get password() { return this.myValidatedForm.get('password'); }
+  get password() {
+    return this.myValidatedForm.get('password');
+  }
 
-  get accept() { return this.myValidatedForm.get('accept'); }
+  get accept() {
+    return this.myValidatedForm.get('accept');
+  }
 
+  sendForm() {
+    if (this.myValidatedForm.valid) {
+      console.log('Form is valid');
+      this.myValidatedForm.reset();
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
-
-
