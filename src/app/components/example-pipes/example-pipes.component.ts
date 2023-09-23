6ab2ad8c2afdd13b40982924a3a09ac25147a68c
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-23 18:03:13 +0200

    corregido

--- src/app/components/example-pipes/example-pipes.component.ts
+++ src/app/components/example-pipes/example-pipes.component.ts
@@ -3,25 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-example-pipes',
   templateUrl: './example-pipes.component.html',
-  styleUrls: ['./example-pipes.component.css']
+  styleUrls: ['./example-pipes.component.css'],
 })
 export class ExamplePipesComponent implements OnInit {
+  dob: Date = new Date(1990, 5, 20);
+  change: boolean = true;
 
-    dob: Date = new Date(1990, 5, 20);
-    change: boolean = true;
+  constructor() {}
 
-    constructor() { }
+  ngOnInit() {}
 
-    get dateFormat() {
-      return this.change ? 'shortDate' : 'fullDate';
-    }
+  get dateFormat() {
+    return this.change ? 'shortDate' : 'fullDate';
+  }
 
-    ngOnInit() {
-      this.change = !this.change;
-    }
-
-    changeDate = () => {
-      this.change = !this.change;
-    }
-
+  changeFormat() {
+    this.change = !this.change;
+  }
 }
