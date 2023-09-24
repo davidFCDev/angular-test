d5a4bbe8c7903cbf7d0362ab485bc38a4475ab22
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-24 16:38:07 +0200

    upper/lower

--- src/app/components/example-pipes/example-pipes.component.ts
+++ src/app/components/example-pipes/example-pipes.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ExamplePipesComponent implements OnInit {
   dob: Date = new Date(1990, 5, 20);
   change: boolean = true;
+  name: string = 'John Doe';
 
   constructor() {}
 
