bd8bc0acbed10135f9999acde827486af5509ff8
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-03 16:09:17 +0200

    animacion

--- src/app/components/ej-animation/ej-animation.component.ts
+++ src/app/components/ej-animation/ej-animation.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-ej-animation',
   templateUrl: './ej-animation.component.html',
-  styleUrls: ['./ej-animation.component.css']
+  styleUrls: ['./ej-animation.component.css'],
+  // Metadatos para animacion de componente
+  animations: [
+    trigger('animaciones', [
+      state('inicial', style({ backgroundColor: 'red' })),
+      state('final', style({ backgroundColor: 'blue' })),
+      transition('inicial => final', animate('1000ms ease-in')),
+      transition('final => inicial', animate('1000ms ease-out')),
+    ]),
+  ],
 })
-export class EjAnimationComponent {
+export class EjAnimationComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {}
 }
